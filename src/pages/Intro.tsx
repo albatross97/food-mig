217215e07e3a58f08eb53b89fd6460129c0a88b9
albatross97 <ruiwang704@gmail.com>
2022-02-04 18:26:38 -0500

    font style

---
+++ src/pages/Intro.tsx
@@ -0,0 +1,17 @@
+const Intro = () => {
+  return (
+    <div className="u-section intro">
+      <div className="header-container">
+        <h1>Food Security and Migration</h1>
+        <p>
+          A Data Visualization Story in Northern Triangle Region of Central
+          America
+        </p>
+
+        <small>SCROLL TO CONTINUE</small>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
